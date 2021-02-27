0c9d11d8c642781d9496ff4deb1b4804936effee
Brian Sturgis <sturujisu@gmail.com>
2021-02-27 13:27:25 -0800

    pass test for Bread_Object_Bread

--- Bakery.Tests/ModelTests/Bakery.Tests.cs
+++ Bakery.Tests/ModelTests/Bakery.Tests.cs
@@ -2,9 +2,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Bakery.Models;
 namespace Bakery.Tests
-
+{
   [TestClass]
     public class BakeryTests
     {
 
+
+      [TestMethod]
+      public void Bread_Object_Bread()
+      {
+       Bread newBread = new Bread(1);
+       Assert.AreEqual(typeof(Bread), newBread.GetType());
+      }
+
+      [TestMethod]
+      public void Bread_BreadQuantity_Int()
+      {
+        
+      }
+
     }
+}

6d73a3ee0f7bfe98c91a05ab1fe14a8a0b2e05dd
Brian Sturgis <sturujisu@gmail.com>
2021-02-27 12:47:19 -0800

    update all cs files with boiler plate and first two variables

---
+++ Bakery.Tests/ModelTests/Pastry.Tests.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+namespace Bakery.Tests
+
+  [TestClass]
+  public class PastryTests
+  {
+  }
---
+++ Bakery/Models/Bakery.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Bread 
+  {
+    public int Quantity { get; set; }
+    public int Total { get; set; }
+
+    
+  }
+}
